072dc822ea412cfc86e49bc2b69f2f1755ebe954
chao140 <peterhuang24@gmail.com>
2020-02-14 19:33:16 +0100

    Febrary14
    Spriteanimation_01

---
+++ AVD/Assets/playercontrol2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol2D : MonoBehaviour
+{
+    public float runSpeed = 40f;
+    [SerializeField]
+    float horizontalMove = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+}
