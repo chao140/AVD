18b2b56ec30cc25b3ac00f2bcd1a9bcde9a8a982
chao140 <peterhuang24@gmail.com>
2020-02-27 22:47:04 +0100

    it works
    finally

---
+++ AVD/Assets/cherry.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cherry : MonoBehaviour
+{
+public GameObject deathEffect;
+
+   void OnCollisionEnter2D(Collision2D col)
+{
+	if (col.gameObject.tag == "cherry")
+	{
+		Destroy(col.gameObject);
+        Die();
+	}
+    void Die ()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+       
+    }
+}
+}
